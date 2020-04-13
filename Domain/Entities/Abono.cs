3874ec02ae05e21db98af5ffac7f2f5ef38a62df
fabiandres2000 <grovveip@gmail.com>
2020-04-13 18:55:35 -0500

    primer commit

---
+++ Domain/Entities/Abono.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Abono
+    {
+        public string Cedula { get; set; }
+        public double ValorAbonado { get; set; }
+        public DateTime FechaAbono { get; set; }
+    }
+}
